be4a90dc83d0ad257255942c2365670ad110701e
danielkyman <danny.kyman@gmail.com>
2020-08-31 16:35:01 -0700

    adding responsive breakpoints

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -56,33 +56,34 @@ const Contact = () => {
             </div>
             <input type="submit" />
           </form>
-        </div>
-        <div className="references">
-          <h5>References</h5>
-          <p>
-            “Clever rockstar of a coder with a magnetic personality and the
-            strongest work ethic you will ever find in a candidate.”
-          </p>
-          <cite>-Anthony K. (Team Lead)</cite>
-          <p>
-            “Daniel is someone who always pushes for the best. He would be the
-            first person I would think to partner with on any project and he is
-            an ideal team mate.”
-          </p>
-          <cite>-Vanshika P. (Peer/Colleague)</cite>
-          <p>
-            “Daniel embraces challenges instead of avoiding them. When he worked
-            on our team, he always insisted on doing things properly. He refused
-            to take the easy way out. I admire his determination to maintain
-            high standards and never compromise the project’s integrity.”
-          </p>
-          <cite>-Zahid K. (Peer/Colleague)</cite>
-          <p>
-            “Daniel is one of the strongest coders that I have met in my time at
-            Lambda. I think he is a tremendously hard worker and an asset for
-            any team.”
-          </p>
-          <cite>-Mike C. (Peer/Colleague)</cite>
+          <div className="references">
+            <h5>References</h5>
+            <p>
+              “Clever rockstar of a coder with a magnetic personality and the
+              strongest work ethic you will ever find in a candidate.”
+            </p>
+            <span>-Anthony K. (Team Lead)</span>
+            <p>
+              “Daniel is someone who always pushes for the best. He would be the
+              first person I would think to partner with on any project and he
+              is an ideal team mate.”
+            </p>
+            <span>-Vanshika P. (Peer/Colleague)</span>
+            <p>
+              “Daniel embraces challenges instead of avoiding them. When he
+              worked on our team, he always insisted on doing things properly.
+              He refused to take the easy way out. I admire his determination to
+              maintain high standards and never compromise the project’s
+              integrity.”
+            </p>
+            <span>-Zahid K. (Peer/Colleague)</span>
+            <p>
+              “Daniel is one of the strongest coders that I have met in my time
+              at Lambda. I think he is a tremendously hard worker and an asset
+              for any team.”
+            </p>
+            <span>-Mike C. (Peer/Colleague)</span>
+          </div>
         </div>
       </div>
     </div>
